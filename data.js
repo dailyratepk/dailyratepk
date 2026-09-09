92e8d26e328031a12d019c2dd5651db98ce503bb
Noman <nomansharif173@gmail.com>
2026-09-09 21:19:04 +0500

    Initial DailyRatePK website

---
+++ data.js
@@ -0,0 +1,26 @@
+const DAILY_RATES = {
+  updated: "Last verified update: September 9, 2026",
+
+  fuel: {
+    petrol: 364.35,
+    diesel: 385.95,
+    lpg: 258.65
+  },
+
+  metals: {
+    gold24kTola: 462936,
+    gold24k10g: 396893,
+    gold24kGram: 39689.30,
+    gold22kTola: 424372.48,
+    silverTola: 7141,
+    silver10g: 6122,
+    silverGram: 612.20
+  },
+
+  currency: {
+    usd: 277.80,
+    aed: null,
+    sar: null,
+    gbp: null
+  }
+};
